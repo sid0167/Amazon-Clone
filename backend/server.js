5b4c6686426a75ccdbdf550f8abf25de66cf0fa8
sid0167 <siddang2005@gmail.com>
2026-04-15 22:26:29 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,9 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
+app.get("/api", (req, res) => {
+  res.send("API is running 🚀");
+});
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
